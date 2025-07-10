663b4ce70cae03555901e15e3c279c61256ba108
sardamed <cbamiixsimo@gmail.com>
2025-07-10 12:13:21 +0100

    dashboard responsive

--- taqathon-front/src/components/dashboard/ServiceFilter.tsx
+++ taqathon-front/src/components/dashboard/ServiceFilter.tsx
@@ -18,17 +18,20 @@ const services: ServiceOption[] = [
   { id: 5, name: "EL", label: "EL" },
 ];
 
-export const ServiceFilter = ({ currentService, onServiceChange }: ServiceFilterProps) => {
+export const ServiceFilter = ({
+  currentService,
+  onServiceChange,
+}: ServiceFilterProps) => {
   return (
-    <div className="">
-      <p className="text-xl md:text-2xl lg:text-3xl font-medium text-black mb-4 md:mb-6">
+    <div className="w-full">
+      <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-medium text-black mb-3 sm:mb-4 md:mb-6">
         Filtre par métier :
       </p>
-      <div className="flex gap-6 w-full items-center">
+      <div className="flex flex-wrap gap-2 sm:gap-3 md:gap-4 lg:gap-6 w-full items-center">
         {services.map((service) => (
           <div
             key={service.id}
-            className={`bg-[#003d553b] text-white px-4 py-2 rounded-md cursor-pointer transition-all duration-300 ${
+            className={`bg-[#003d553b] text-white px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-3 rounded-md cursor-pointer transition-all duration-300 text-sm sm:text-base md:text-lg hover:bg-[#003D55] ${
               currentService === service.name ? "!bg-[#003D55]" : ""
             }`}
             onClick={() => onServiceChange(service.name)}
@@ -39,4 +42,4 @@ export const ServiceFilter = ({ currentService, onServiceChange }: ServiceFilter
       </div>
     </div>
   );
-}; 
+};
